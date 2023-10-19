5603718fa332f1dd30f5d9a218b0f2c0207dcb71
Robin Wu <robin.wu@kaiostech.com>
2023-10-19 13:37:14 +0800

    Add comments for functions.

--- db.js
+++ db.js
@@ -1,82 +1,30 @@
 const pg = require("pg");
 
+// postgres client settings
 const pool = new pg.Pool({
   host: "localhost",
   user: "admin",
   database: "jubo",
   password: "jubopwd",
   port: 5432,
-  max: 20,  
+  max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 });
 
-const CHECK_PATIENTS_EXISTS = `
-SELECT EXISTS (
-  SELECT 1
-  FROM   information_schema.tables 
-  WHERE  table_schema = 'public'
-  AND    table_name = 't_patients'
-);`;
-
-const CHECK_ORDER_EXISTS = `
-SELECT EXISTS (
-  SELECT 1
-  FROM   information_schema.tables 
-  WHERE  table_schema = 'public'
-  AND    table_name = 't_orders'
-);`;
-
-const CREATE_PATIENTS_TABLE = `
-CREATE TABLE "t_patients" (
-  "id" SERIAL PRIMARY KEY,
-  "name" VARCHAR(50)NOT NULL,
-  "telephone" VARCHAR(20)NOT NULL,
-  "address" VARCHAR(100)NOT NULL
-);`;
-
-const INIT_PATIENTS = `
-INSERT INTO "t_patients"
-  ("name", "telephone", "address")
-VALUES
-  ('王大明', '0911999888', '新北市新店區民權路99號'),
-  ('林小華', '0937199347', '新北市新店區中正路29號5樓'),
-  ('方大同', '0912389108', '新北市新店區建國路299號之1'),
-  ('李小龍', '0936635151', '新北市新店區北新路三段60巷47號'),
-  ('包不同', '0921384448', '新北市新店區建國路199號');
-`;
-
-const CREATE_ORDER_TABLE = `
-CREATE TABLE "t_orders" (
-  "id" SERIAL PRIMARY KEY,
-  "patient_id" INT NOT NULL,
-  "message" VARCHAR(200)NOT NULL,
-  "create_time" TIMESTAMPTZ NOT NULL,
-CONSTRAINT fk_patient
-  FOREIGN KEY(patient_id) 
-    REFERENCES t_patients(id)
-);`;
-
-const INIT_ORDERS = `
-INSERT INTO "t_orders"
-  ("patient_id", "message", "create_time")
-VALUES
-  (1, '超過120請施打8u', '2023-10-15 16:30:00'),
-  (2, '就寢時間，臨睡前大劑量給藥', '2023-10-16 14:09:00'),
-  (3, '繼續同樣治療', '2023-10-15 19:47:00'),
-  (4, '靜脈注射含5%葡萄糖的生理鹽水', '2023-10-13 20:16:00'),
-  (5, '禁食，禁飲水', '2023-10-18 15:53:00');
-`;
-
+/**
+ * The common postgres query function.
+ * @param {string} queryString - The postgreSQL query syntax.
+ */
 const queryPostgre = async (queryString) => {
   const client = await pool.connect();
   let result = null;
-  
-  try {  
+
+  try {
     result = await client.query(queryString);
-    
+
     if (!result) {
-      console.error('Query result is undefined or null.');
+      console.error("Query result is undefined or null.");
     } else {
       return result;
     }
@@ -87,44 +35,117 @@ const queryPostgre = async (queryString) => {
   }
 };
 
-const initialPatients = async () => {
-  try {
-    const { rows } = await queryPostgre(CHECK_PATIENTS_EXISTS);
-    if (rows && rows[0]) {
-      const { exists } = rows[0];
+/**
+ * Check if the tableName exists in the postgreSQL.
+ * @param {string} tableName
+ * @return {boolean}
+ */
+const checkTableExist = async (tableName) => {
+  const checkExistSQL = `
+    SELECT EXISTS (
+      SELECT 1
+      FROM   information_schema.tables 
+      WHERE  table_schema = 'public'
+      AND    table_name = '${tableName}'
+    );`;
 
-      if (!exists) {
-        await queryPostgre(CREATE_PATIENTS_TABLE);
-        await queryPostgre(INIT_PATIENTS);
-      }
-    }
-  } catch (error) {
-    console.error(error.stack);
-  }
-};
-const initialOrders = async () => {
   try {
-    const { rows } = await queryPostgre(CHECK_ORDER_EXISTS);
+    const { rows } = await queryPostgre(checkExistSQL);
     if (rows && rows[0]) {
-      const { exists } = rows[0];
-
-      if (!exists) {
-        await queryPostgre(CREATE_ORDER_TABLE);
-        await queryPostgre(INIT_ORDERS);
-      }
+      return rows[0].exists;
     }
   } catch (error) {
     console.error(error.stack);
   }
 };
 
+/**
+ * Create table "t_patients" and give initial data.
+ */
+const initialPatientsTable = async () => {
+  const createTableSQL = `
+    CREATE TABLE "t_patients" (
+      "id" SERIAL PRIMARY KEY,
+      "name" VARCHAR(50)NOT NULL,
+      "telephone" VARCHAR(20)NOT NULL,
+      "address" VARCHAR(100)NOT NULL
+    );`;
+  const initDataSQL = `
+    INSERT INTO "t_patients"
+      ("name", "telephone", "address")
+    VALUES
+      ('王大明', '0911999888', '新北市新店區民權路99號'),
+      ('林小華', '0937199347', '新北市新店區中正路29號5樓'),
+      ('方大同', '0912389108', '新北市新店區建國路299號之1'),
+      ('李小龍', '0936635151', '新北市新店區北新路三段60巷47號'),
+      ('包不同', '0921384448', '新北市新店區建國路199號');`;
+
+  try {
+    const tableName = "t_patients";
+    const exists = await checkTableExist(tableName);
+
+    if (!exists) {
+      await queryPostgre(createTableSQL);
+      await queryPostgre(initDataSQL);
+    }
+  } catch (error) {
+    console.error(error.stack);
+  }
+};
+
+/**
+ * Create table "t_orders" and give initial data.
+ */
+const initialOrdersTable = async () => {
+  const createTableSQL = `
+    CREATE TABLE "t_orders" (
+      "id" SERIAL PRIMARY KEY,
+      "patient_id" INT NOT NULL,
+      "message" VARCHAR(200)NOT NULL,
+      "create_time" TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_patient
+      FOREIGN KEY(patient_id) 
+        REFERENCES t_patients(id)
+    );`;
+
+  const initDataSQL = `
+    INSERT INTO "t_orders"
+      ("patient_id", "message", "create_time")
+    VALUES
+      (1, '超過120請施打8u', '2023-10-15 16:30:00'),
+      (2, '就寢時間，臨睡前大劑量給藥', '2023-10-16 14:09:00'),
+      (3, '繼續同樣治療', '2023-10-15 19:47:00'),
+      (4, '靜脈注射含5%葡萄糖的生理鹽水', '2023-10-13 20:16:00'),
+      (5, '禁食，禁飲水', '2023-10-18 15:53:00');
+    `;
+
+  try {
+    const tableName = "t_orders";
+    const exists = await checkTableExist(tableName);
+
+    if (!exists) {
+      await queryPostgre(createTableSQL);
+      await queryPostgre(initDataSQL);
+    }
+  } catch (error) {
+    console.error(error.stack);
+  }
+};
+
+/**
+ * Initialize table schema and data once the service boots in the first time.
+ */
 const initialDatabase = async () => {
-  await initialPatients();
-  await initialOrders();
+  await initialPatientsTable();
+  await initialOrdersTable();
 };
 
+/**
+ * Get all patients information from table.
+ * @returns {array} patient list
+ */
 const getAllPatients = async () => {
-  const query = "SELECT * from t_patients";
+  const query = "SELECT * FROM t_patients";
   try {
     const { rows } = await queryPostgre(query);
     return rows;
@@ -132,55 +153,69 @@ const getAllPatients = async () => {
     console.error(error.stack);
   }
 };
+
+/**
+ * Get order list by patient_id
+ * @param {int} patientId
+ * @return {array} order list
+ */
 const getAllOrdersFromPatientId = async (patientId) => {
-  const query = 
-  `SELECT id, row_number() over() as sno, message from t_orders 
+  const query = `
+    SELECT id, row_number() over() as sno, message 
+    FROM t_orders 
     WHERE patient_id = ${patientId} 
-    ORDER BY create_time
-  `;
+    ORDER BY create_time`;
+
   try {
     const { rows } = await queryPostgre(query);
     return rows;
   } catch (error) {
     console.error(error.stack);
   }
-}
+};
 
+/**
+ * Add an order into the table.
+ * @param {int} patientId - patient ID
+ * @param {string} message - order message
+ */
 const insertOrderByPatientId = async (patientId, message) => {
-  const now = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '');
+  const now = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
   const query = `
     INSERT INTO "t_orders"
       ("patient_id", "message", "create_time")
     VALUES
-      (${patientId}, '${message}', '${now}');
-  `;
+      (${patientId}, '${message}', '${now}');`;
 
   try {
     await queryPostgre(query);
   } catch (error) {
     console.error(error.stack);
   }
-}
+};
 
-const updateOrderByOrderId = async (orderId, message) => {
-  const query = `
-    UPDATE t_orders
-    SET message='${message}'
-    WHERE id='${orderId}';
-  `;
+/**
+ * Update an exists order with order ID and order message.
+ * @param {int} orderId - order ID
+ * @param {string} message - order message
+ */
+const updateOrderById = async (orderId, message) => {
+  const query = `UPDATE t_orders SET message='${message}' WHERE id='${orderId}';`;
 
   try {
     await queryPostgre(query);
   } catch (error) {
     console.error(error.stack);
   }
-}
+};
 
+/**
+ * Remove an exists order from the table.
+ * @param {int} orderId
+ */
 const deleteOrderById = async (orderId) => {
-  const query = `
-    DELETE FROM t_orders
-    WHERE id=${orderId}
-  `;
+  const query = `DELETE FROM t_orders WHERE id=${orderId}`;
+
   try {
     await queryPostgre(query);
   } catch (error) {
@@ -193,6 +228,6 @@ module.exports = {
   getAllPatients,
   getAllOrdersFromPatientId,
   insertOrderByPatientId,
-  updateOrderByOrderId,
-  deleteOrderById
+  updateOrderById,
+  deleteOrderById,
 };
--- main.js
+++ main.js
@@ -1,58 +1,102 @@
 const express = require("express");
+const {
+  deleteOrderById,
+  getAllPatients,
+  getAllOrdersFromPatientId,
+  initialDatabase,
+  insertOrderByPatientId,
+  updateOrderById,
+} = require("./db");
+
 const app = express();
-const { initialDatabase, getAllPatients, getAllOrdersFromPatientId, insertOrderByPatientId, updateOrderByOrderId, deleteOrderById } = require('./db');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/getPatients", async (req, res) => {
-  console.log(req.originalUrl);
-  const results = await getAllPatients();
-  res.status(200).json(results);
+/**
+ * Fetch all patient list.
+ * Endpoint: [GET] http://localhost:8081/getPatients
+ */
+app.get("/getPatients", async (_, res) => {
+  try {
+    const results = await getAllPatients();
+    res.status(200).json(results);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
+/**
+ * Fetch order list of the specific patient.
+ * Endpoint: [GET] http://localhost:8081/order?patientId={0}
+ */
 app.get("/order", async (req, res) => {
-  console.log(req.originalUrl);
-  const results = await getAllOrdersFromPatientId(req.query.patientId);
-  res.status(200).json(results);
+  try {
+    const results = await getAllOrdersFromPatientId(req.query.patientId);
+    res.status(200).json(results);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-app.post('/order', async (req, res) => {
+/**
+ * Add or update an order data.
+ * Endpoint: [POST] http://localhost:8081/order
+ *
+ * - Add
+ *  Request Body: {
+ *    "isNew": true,
+ *    "patientId": 3,
+ *    "message": "This is a new order message."
+ *  }
+ * - Update
+ *  Request Body: {
+ *    "orderId": 8,
+ *    "message": "I'd like to modify the order message."
+ *  }
+ */
+app.post("/order", async (req, res) => {
   const { patientId, orderId, message, isNew } = req.body;
-  
+
   try {
     if (isNew) {
       // insert
       await insertOrderByPatientId(patientId, message);
     } else {
       // update
-      await updateOrderByOrderId(orderId, message);
+      await updateOrderById(orderId, message);
     }
     res.send({
-      success: 1, 
-      message: `${isNew ? 'INSERT' : 'UPDATE'} Complete!`,
+      success: 1,
+      message: `${isNew ? "INSERT" : "UPDATE"} Complete!`,
     });
   } catch (err) {
     res.send({
-      success: 0, 
+      success: 0,
       message: err,
     });
   }
 });
 
-app.delete('/order', async (req, res) => {
+/**
+ * Delete an order from the table.
+ * Endpoint: [DELETE] http://localhost:8081/order?orderId={0}
+ */
+app.delete("/order", async (req, res) => {
   const { orderId } = req.query;
   await deleteOrderById(orderId);
   res.send({
-    success: 1, 
-    message: "DELETE Complete!"
+    success: 1,
+    message: "DELETE Complete!",
   });
-})
+});
 
+// Start the express RESTful API services @ http://localhost:8081
 app.listen(8081, () => {
-  console.log("http://localhost:8081/");
+  console.log("RESTful APIs can be accessed from http://localhost:8081/");
 });
 
 (async () => {
+  // Check and initialize the database schema
   await initialDatabase();
 })();
